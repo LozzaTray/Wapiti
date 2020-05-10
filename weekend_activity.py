3c58dada976e6530e2e28f60302a613cceeca21d
Lawrence Tray <ltray99@gmail,com>
2020-05-10 18:57:26 +0100

    WAP-1 Add output module

--- weekend_activity.py
+++ weekend_activity.py
@@ -1,6 +1,7 @@
 """Code for decoding data"""
 from packages.parser import read_csv_as_array
 from packages.ofdm import decode
+from packages.output import write_to_file
 import os
 
 curr_dir = os.path.dirname(__file__)
@@ -15,6 +16,8 @@ def run():
     channel = read_csv_as_array(channel_file)
 
     decoded_sequence = decode(data_seq, channel)
+    output_file = os.path.join(curr_dir, "output/wa1.txt")
+    write_to_file(output_file, decoded_sequence)
     print(decoded_sequence)
 
 
