d9dd54eac63e0e64d290530c57f56ab43441401d
EJK52 <ejk52@cam.ac.uk>
2020-05-10 19:57:49 +0100

    symbol to bits code - should be alright

--- packages/decoder.py
+++ packages/decoder.py
@@ -1,8 +1,17 @@
 """Module for decoding"""
+import numpy
 
 
 def map_symbol_to_bits(complex_symbol):
     """Checks argument and or magnitude and returns"""
+    arg = numpy.angle(complex_symbol, deg=True)
+    mag = numpy.abs(complex_symbol)
+    if mag < 0.01: symbol = 2
+    if (arg >= 0) and (arg <= 90): symbol = [0, 0]
+    if (arg >= 90) and (arg <= 180): symbol = [0, 1]
+    if (arg <= 0) and (arg >= -90): symbol = [1, 1]
+    if (arg <= 90) and (arg >= -180): symbol = [1, 0]
+    return symbol
 
 
 def decode_symbol_sequence(symbol_sequence):
