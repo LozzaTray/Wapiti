68db9934797a9899aa71ed6384aab56d98c9a1d7
Lawrence Tray <ltray99@gmail.com>
2020-05-10 10:29:05 +0100

    WAP Scaffold for OFDM file

---
+++ ofdm.py
@@ -0,0 +1,19 @@
+"""Module for performing ofdm tasks"""
+
+
+def insert_cyclic_prefix(x, K):
+    """inserts a cyclic prefix of length K into the data sequence x
+    allows linear convolution to match cyclic"""
+    raise NotImplementedError
+
+
+def encode(data_sequence, K):
+    """encodes the data_sequence X into the the time sequence x 
+    by taking the iDFT and inserting a cyclic prefix of length K"""
+    raise NotImplementedError
+
+
+def decode(received_sequence, channel_impulse_response, N, K):
+    """decodes by taking the DFT of the received_sequence and channel_impulse_response
+    then apply point-wise division to get the true symbol value"""
+    raise NotImplementedError
