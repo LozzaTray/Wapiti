e92d732d1b3b153cc74da9b6853a5af72e6ec6e0
Lawrence Tray <ltray99@gmail,com>
2020-05-10 19:46:26 +0100

    WAP-1 Scaffold decoder

---
+++ packages/decoder.py
@@ -0,0 +1,9 @@
+"""Module for decoding"""
+
+
+def map_symbol_to_bits(complex_symbol):
+    """Checks argument and or magnitude and returns"""
+
+
+def decode_symbol_sequence(symbol_sequence):
+    """takes whole sequence and maps to bits"""
