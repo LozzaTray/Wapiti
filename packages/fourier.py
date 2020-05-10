74879e20aee4074e89f778dac07a5506c949e689
Kiran Jolly <kiran.jolly@gmail.com>
2020-05-10 19:07:50 +0100

    Fourier

--- packages/fourier.py
+++ packages/fourier.py
@@ -1,17 +1,24 @@
 """Module for performing fourier analysis"""
 
+import numpy
 
-def dft(data_vector, N):
+def dft(data_vector):
     """
     Perform dft on the given data_vector
     Returns vector of length N
     """
+
+    return numpy.fft.fft(data_vector)
+
     raise NotImplementedError
 
 
-def idft(data_vector, N):
+def idft(data_vector):
     """
     Performs inverse dft on the given data_vector
     Returns vector of length N
     """
+
+    return numpy.fft.ifft(data_vector)
+
     raise NotImplementedError
