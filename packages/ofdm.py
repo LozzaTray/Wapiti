4636096cac2ea51f6f4188038af4bf6f19982c78
Lawrence Tray <ltray99@gmail.com>
2020-05-11 14:53:48 +0100

    WAP-1 Successfully decoded - sorry

--- packages/ofdm.py
+++ packages/ofdm.py
@@ -22,17 +22,22 @@ def demodulate(received_sequence, channel_impulse_response, N=1024, K=32):
     sequence_length = len(received_sequence)
     num_blocks = int(sequence_length / (N + K))
 
-    decoded_sequence = np.empty(0)
+    decoded_sequence = []
 
     for i in range(0, num_blocks):
-        lower_index = i*(N+K) + K + 1
+        # discard cyclic prefix
+        lower_index = i*(N+K) + K
         upper_index = lower_index + N
         block = received_sequence[lower_index : upper_index]
 
+        # take dft of block without cyclic pref
         dfted_block = dft(block, N)
         decoded_block = np.divide(dfted_block, H_arr)
+
+        # only take positive frequency bins
         relevant_bins = decoded_block[1 : int(N/2)]
 
+        #concatenate
         decoded_sequence = np.concatenate((decoded_sequence, relevant_bins))
 
     return decoded_sequence
