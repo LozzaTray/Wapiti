c1320855e39c2731362d919eb374125ae5870733
Lawrence Tray <ltray99@gmail.com>
2020-05-11 15:40:03 +0100

    WAP-1 Refactor decoder to make more user-friendly

--- packages/decoder.py
+++ packages/decoder.py
@@ -3,8 +3,11 @@ import numpy
 import math
 
 
-def map_symbol_to_bits(complex_symbol):
-    """Checks argument and or magnitude and returns"""
+def decode_symbol(complex_symbol):
+    """
+    Checks argument and or magnitude and returns
+    """
+
     arg = numpy.angle(complex_symbol, deg=True)
 
     if (arg >= 0) and (arg <= 90):
@@ -20,19 +23,71 @@ def map_symbol_to_bits(complex_symbol):
 
 
 def map_bit_array_to_byte(bit_array):
+    """
+    Convert an array of 8-bits into a byte
+    """
+
+    if len(bit_array) != 8:
+        raise ValueError("Supplied array was not length 8")
+
     byte = ""
     for bit in bit_array:
         byte = byte + str(bit)
     return int(byte, base=2)
 
 
+def parse_byte_sequence(byte_sequence):
+    """
+    parses a byte sequence to retrieve the title, 
+    file length in bytes and byte array corresponding to the file
+    """
+
+    title = ""
+    file_length = ""
+
+    num_bytes = len(byte_sequence)
+    first_zero_index = 0
+    second_zero_index = 0
+
+    for i in range(0, num_bytes):
+        byte = byte_sequence[i]
+        if byte == 0:
+            first_zero_index = i
+            break
+        else:
+            title = title + chr(byte)
+
+    for j in range(first_zero_index + 1, num_bytes):
+        byte = byte_sequence[j]
+        if byte == 0:
+            second_zero_index = j
+            break
+        else:
+            file_length = file_length + chr(byte)
+
+    file_length = int(file_length)
+
+    file_start_index = second_zero_index + 1
+    file_bytes = byte_sequence[file_start_index : file_start_index + file_length]
+
+    return title, file_length, file_bytes
+
+
 def decode_symbol_sequence(symbol_sequence):
-    """loops through sequence until a two is found"""
-    bit_sequence = numpy.array(list(map(map_symbol_to_bits, symbol_sequence)))
+    """
+    decodes the symbol sequence
+    returns: title (str), file_length (int), file_bytes (byte[])
+    """
+
+    bit_sequence = numpy.array(list(map(decode_symbol, symbol_sequence)))
     bit_sequence = bit_sequence.flatten()
     
     num_bits = len(bit_sequence)
     num_bytes = math.floor(num_bits / 8)
-    byte_sequence = numpy.reshape(bit_sequence[0: num_bytes * 8], (num_bytes, 8))
-    byte_sequence = numpy.array(list(map(map_bit_array_to_byte, byte_sequence)))
-    return byte_sequence.astype("uint8")
+
+    bit_sequence = bit_sequence[0: num_bytes * 8]
+
+    byte_sequence = numpy.reshape(bit_sequence, (num_bytes, 8))
+    byte_sequence = numpy.array(list(map(map_bit_array_to_byte, byte_sequence))).astype("uint8")
+    
+    return parse_byte_sequence(byte_sequence)
--- weekend_activity.py
+++ weekend_activity.py
@@ -10,31 +10,30 @@ curr_dir = os.path.dirname(__file__)
 
 def run():
     """main loop"""
+    group = input("Which group are you: ")
+
     print("Reading data file...")
-    data_seq_file = os.path.join(curr_dir, "data/gr3file.csv")
+    data_seq_file = os.path.join(curr_dir, "data/gr" + group + "file.csv")
     data_seq = read_csv_as_array(data_seq_file)
 
     print("Reading channel file...")
-    channel_file = os.path.join(curr_dir, "data/gr3channel.csv")
+    channel_file = os.path.join(curr_dir, "data/gr" + group + "channel.csv")
     channel = read_csv_as_array(channel_file)
 
     print("OFDM demodulation...")
-    #output_demod_file = os.path.join(curr_dir, "output/demodulated.txt")
     demodulated_sequence = demodulate(data_seq, channel)
-    #write_rows(output_demod_file, demodulated_sequence)
 
     print("QPSK decoding...")
-    output_file = os.path.join(curr_dir, "output/decoded.bin")
-    decoded_bytes = decode_symbol_sequence(demodulated_sequence)
-    decoded = decoded_bytes.astype('uint8')
-    decoded.tofile(output_file)
+    title, file_length, file_bytes = decode_symbol_sequence(demodulated_sequence)
+    print("Successfully decoded: " + title + " (" + str(file_length) + " bytes)")
+    
+    print("Saving file...")
+    output_file = os.path.join(curr_dir, "output/" + title)
+    file_bytes.tofile(output_file)
 
-    print("Extracting raw data...")
-    raw_output_file = os.path.join(curr_dir, "output/raw.wav")
-    raw_data = decoded[17 : 17 + 38638 + 1]
-    raw_data.tofile(raw_output_file)
+    print("Done")
 
 
 if __name__ == "__main__":
-    print("Team Wapiti - WA1")
+    print("\nTeam Wapiti - WA1\n~~~~~~~~~~~~~~\n")
     run()
