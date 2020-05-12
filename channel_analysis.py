564c56978ec191e81b30b84927b59f3bd4d04d57
EJK52 <ejk52@cam.ac.uk>
2020-05-12 12:52:17 +0100

    added some code in a new file to start channel analysis

---
+++ channel_analysis.py
@@ -0,0 +1,31 @@
+"""code for doing the channel analysis"""
+import os
+from packages.fourier import dft, idft
+from packages.audio import record, playback
+import wave
+
+
+curr_dir = os.path.dirname(__file__)
+
+
+def run():
+    """main loop"""
+    file_name_short = input("File name to save (.wav): ")
+    file_name_full = os.path.join(curr_dir, "data/" + file_name_short + ".wav")
+
+    duration = int(input("Duration of recording (seconds): "))
+
+    channel_num = int(input("Number of channels to use (1 or 2): "))
+
+    record(duration, file_name_full, channels=channel_num)
+
+    #playback(file_name_full)
+    file = wave.open(file_name_full, mode='rb')
+    nof = file.getnframes() #gets number of frames
+    frame_bytes = file.readframes(nof)
+    print(dft(frame_bytes, nof))
+
+
+if __name__ == "__main__":
+    print("\nTeam Wapiti - Record\n~~~~~~~~~~~~~~~~~~~\n")
+    run()
