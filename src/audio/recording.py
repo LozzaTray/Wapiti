a4e3c78c0adf1ef81e70e0435999992b2953c373
Lawrence Tray <ltray99@gmail.com>
2020-05-13 12:08:09 +0100

    WAP-3 Create recording class

---
+++ src/audio/recording.py
@@ -0,0 +1,103 @@
+"""module for recording and playing wav objects"""
+import pyaudio
+from src.file_io.wav import read_wav, write_wav
+
+
+class Recording:
+
+    # Default props
+    DEFAULT_RATE = 44100
+    DEFAULT_CHUNK = 1024
+    DEFAULT_CHANNELS = 2
+    DEFAULT_FORMAT = pyaudio.paInt16
+
+
+    def __init__(self, frames, rate, channels, audio_format):
+        self.frames = frames
+        self.rate = rate
+        self.channels = channels
+        self.audio_format = audio_format
+
+
+    @classmethod
+    def from_mic(cls, duration, channels=DEFAULT_CHANNELS, rate=DEFAULT_RATE, chunk=DEFAULT_CHUNK, audio_format=DEFAULT_FORMAT):
+        """Initialise recording from mic"""
+
+        p = pyaudio.PyAudio() #create pyaudio object
+
+        #start recording
+        stream = p.open(
+            format=audio_format, 
+            channels=channels,
+            rate=rate,
+            frames_per_buffer=chunk,
+            input=True
+        )
+
+        frames = []
+        print ("Recording...")
+
+        #read audio data from stream
+        for i in range(0, int(rate / chunk * duration)):
+            data = stream.read(chunk)
+            frames.append(data)
+        
+        print ("Finished recording")
+    
+        #stop recording
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        
+        return cls(
+            frames=frames,
+            rate=rate,
+            channels=channels,
+            audio_format=audio_format
+        )
+
+
+    @classmethod
+    def from_file(cls, original_file):
+        """Initialise recording from file"""
+        data_sequence, sample_width, num_channels, frame_rate = read_wav(original_file)
+
+        return cls(
+            frames=data_sequence,
+            rate=frame_rate,
+            channels=num_channels,
+            audio_format=pyaudio.get_format_from_width(sample_width)
+        )
+
+
+    def play(self):
+        """Plays the specified recording"""
+        #create pyaudio object
+        p = pyaudio.PyAudio() 
+        
+        #open audio stream
+        stream = p.open(
+            format = self.audio_format,
+            channels = self.channels,
+            rate = self.rate,
+            output = True
+        )
+
+        # write to stream one block at a time
+        for frame in self.frames:
+            stream.write(frame)
+        
+        #cleanup
+        stream.close()    
+        p.terminate()
+
+
+    def save(self, filename):
+        """Saves current recording at the specified file"""
+        write_wav(
+            filename=filename,
+            frames=self.frames,
+            channels=self.channels,
+            rate=self.rate,
+            wav_format=self.audio_format
+        )
--- src/file_io/wav.py
+++ src/file_io/wav.py
@@ -1,7 +1,9 @@
 """module for reading and writing wav files"""
 import wave
+import pyaudio
 
-def read_wav(filename):
+
+def read_wav_full(filename):
     """reads in a wav file"""
     file = wave.open(filename, mode='rb')
     frames = file.readframes(file.getnframes())
@@ -9,6 +11,42 @@ def read_wav(filename):
     return frames, file_params[0:4]
 
 
-def write_wav(filename):
+def read_wav(filename, chunk=1024):
+    """
+    Reads in a wav file
+    Accepts:
+        filename (string)
+        chunk_size (int?)
+    Returns:
+        data_sequence (?), 
+        sample_width (in), 
+        num_channels (int), 
+        frame_rate (int)
+    """
+
+    wf = wave.open(filename, 'rb') #open file for reading
+    p = pyaudio.PyAudio() #create pyaudio object
+
+    data_sequence = []
+    data = wf.readframes(chunk) #read data
+    
+    while data != b'':
+        data = wf.readframes(chunk)
+        data_sequence.append(data)
+    
+    p.terminate()
+
+    return data_sequence, wf.getsampwidth(), wf.getnchannels(), wf.getframerate()
+
+
+def write_wav(filename, frames, channels, rate, wav_format):
     """writes out a wav file"""
-    raise NotImplementedError
+    #create file from datastream
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(pyaudio.get_sample_size(wav_format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    
+    #clean up
+    wf.close()
