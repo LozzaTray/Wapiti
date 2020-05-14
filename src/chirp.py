6894aa2090c9eba8c183dda84ba971c06f07e96f
Kiran Jolly <kiran.jolly@gmail.com>
2020-05-14 11:52:26 +0100

    WAP-5 Chirp function

--- src/chirp.py
+++ src/chirp.py
@@ -7,38 +7,14 @@ import pyaudio
 
 def run():
     
-#    p = pyaudio.PyAudio()
-#
-#    volume = 0.5     # range [0.0, 1.0]
-#    fs = 48000       # sampling rate, Hz, must be integer
-#    duration = 1.0   # in seconds, may be float
-#    f = 10000.0        # sine frequency, Hz, may be float
-#    
-#    # generate samples, note conversion to float32 array
-#    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
-#    
-#    # for paFloat32 sample values must be in range [-1.0, 1.0]
-#    stream = p.open(format=pyaudio.paFloat32,
-#                    channels=1,
-#                    rate=fs,
-#                    output=True)
-#    
-#    # play. May repeat with different volume values (if done interactively) 
-#    stream.write(volume*samples)
-#    
-#    stream.stop_stream()
-#    stream.close()
-#
-#    p.terminate()
-    
     t1 = 5
     f0 = 400
     f1 = 10000
-    fs = 48000
+    fs = 192000
     t = np.linspace(0, t1, fs*t1)
-    data = (scipy.signal.chirp(t, f0, t1, f1, method='linear', phi=0, vertex_zero=True)*0.1).astype(np.float16)
-    print(data)
-    plt.plot(data[-100:])
+    data = scipy.signal.chirp(t, f0, t1, f1, method='linear', phi=0, vertex_zero=True).astype(np.float16)
+    #print(data)
+    #plt.plot(data)
     
     recording = Recording.from_list(data, fs)
     recording.play()
