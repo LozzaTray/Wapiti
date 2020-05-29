5ab2c1371f43f85cbfc14cac55d9005c4e64dc9f
Lawrence Tray <ltray99@gmail.com>
2020-05-29 17:33:46 +0100

    WAP-16 More q-tests

--- test/ofdm/test_plug_ofdm.py
+++ test/ofdm/test_plug_ofdm.py
@@ -33,19 +33,19 @@ def test_recovery_real_channel():
     K = 2
     Q1 = 1
     Q2 = 3
+
+    Q = Q2 - Q1
     P = N + K
 
     channel_response = [2, 1]
 
     actual_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    num_packets = len(actual_data) // Q
 
     modulated_sequence = modulate_sequence(actual_data, N, K, Q1, Q2)
 
-    sent_sequence = np.convolve(modulated_sequence, channel_response)[0 : P]
+    sent_sequence = np.convolve(modulated_sequence, channel_response)[0 : num_packets * P]
     demodulated_sequence = demodulate_sequence(sent_sequence, channel_response, N, K, Q1, Q2)
-    just_real_integers = np.round(np.real_if_close(demodulated_sequence))
-
-    data_length = len(actual_data)
-    received_data = just_real_integers[:data_length]
+    received_data = np.round(np.real_if_close(demodulated_sequence))
 
     assert list(received_data) == list(actual_data)
