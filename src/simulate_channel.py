379d5dfec6d899839d9a8e672443ec7e17349e4b
Lawrence Tray <ltray99@gmail.com>
2020-05-29 17:51:19 +0100

    WAP-19 Plugtest complete

--- src/simulate_channel.py
+++ src/simulate_channel.py
@@ -1,13 +1,13 @@
 """Code for simulating a virtual channel"""
 from src.audio.recording import Recording
-from src.file_io.utils import get_output_file_path, get_data_file_path
+from src.file_io.utils import get_recording_file_path
 from src.plotting.plot_recording import plot_recording
 
 
 def run():
     """main loop"""
     signal_file = input("Signal to load (.wav): ")
-    signal_file = get_data_file_path(signal_file + ".wav")
+    signal_file = get_recording_file_path(signal_file + ".wav")
     signal = Recording.from_file(signal_file)
 
     channel = [0.5 , 0.2 , 0, 0.1, 0.04, 0]
@@ -17,7 +17,7 @@ def run():
     print("Done")
 
     out_file = input("File name to save (.wav): ")
-    out_file = get_output_file_path(out_file + ".wav")
+    out_file = get_recording_file_path(out_file + ".wav")
     received_signal.save(out_file)
 
     plot_recording(signal, "Original Signal")
